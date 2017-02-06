54f82f8e83cb2597ae7b8c68f0902f821e58e1e1
Abhi14011998 <abhinavsawantcool@gmail.com>
2017-02-06 22:11:44 +0530

    java Session 3 Assignment1

---
+++ src/Session3Assignment1.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+public class Session3Assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+			Scanner scnr = new Scanner(System.in);
+
+			double num;
+
+		    double numsqrt;
+
+		    double numcbrt;
+
+			
+
+			System.out.println ("Enter the number to find root");
+
+			num= scnr.nextDouble();
+
+			numsqrt = sqrt1(num);
+
+			numcbrt = cbrt1(num);
+
+			System.out.println ("The square root of number is "+numsqrt);
+
+			System.out.println ("The cube root of number is "+numcbrt);
+
+			
+
+			scnr.close();
+
+			}
+
+			
+
+			public static double sqrt1(double number ){
+
+				
+
+				return Math.sqrt(number)	;
+
+				}
+
+			
+
+			public static double cbrt1(double number ){
+
+				
+
+				return Math.cbrt(number)	;
+
+	}
+
+}
